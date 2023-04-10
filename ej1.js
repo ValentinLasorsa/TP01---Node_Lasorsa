0034d823ae637994225b9414306e11cfda55d2b6
VALENTIN JULIAN LASORSA NIKCEVICH <46919212@ort>
2023-04-10 09:24:29 -0300

    j

---
+++ ej1.js
@@ -0,0 +1,7 @@
+const string1 = "Escuela";
+const string2 = "ORT";
+
+const concatenatedString = string1.concat(string2); 
+const invertedString = concatenatedString.split("").reverse().join(""); 
+
+console.log(invertedString); 
